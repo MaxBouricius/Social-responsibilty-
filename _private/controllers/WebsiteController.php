ea2398c194aff280c1d69952a4ca6ee85f8c528d
MaxBouricius <MaxBouricius@hotmail.com>
2021-09-22 15:33:35 +0200

    mvc

--- _private/controllers/WebsiteController.php
+++ _private/controllers/WebsiteController.php
@@ -18,6 +18,11 @@ class WebsiteController {
 		echo $template_engine->render('homepage');
 
 	}
+	public function adminPage() {
 
+		$template_engine = get_template_engine();
+		echo $template_engine->render('adminPage');
+
+	}
 }
 
