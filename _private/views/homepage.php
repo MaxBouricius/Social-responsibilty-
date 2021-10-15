b8c98941ce3d5a918939f8a865e42b47c991f766
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2021-10-15 14:38:38 +0200

    frontpage content

--- _private/views/homepage.php
+++ _private/views/homepage.php
@@ -1,16 +1,81 @@
 <?php $this->layout('layouts::website');?>
 
 <?php $this->start('sidebar')?>
-<!-- Door sections toe te voegen aan je layout kun je deze per pagina aanpassen -->
-<div class="top-10">
-	<h3>Meer weten over...</h3>
-	<ol>
-        <li><a href="https://www.transformers.community/" target="_blank">Transformers Community</a></li>
-        <li><a href="https://www.nji.nl/depressie" target="_blank">Depressie</a></li>
-        <li><a href="http://www.kenniscentrum-kjp.nl/ouders-jongeren/angst/" target="_blank">Angststoornissen</a></li>
-        <li><a href="http://www.kenniscentrum-kjp.nl/ouders-jongeren/gedragsstoornissen-odd-cd/" target="_blank">Gedragsstoornissen</a></li>
-        <li><a href="http://www.trimbos.nl/" target="_blank">Verslaving</a></li>
-	</ol>
+
+<div class="top">
+	<div class="nav">
+
+        </div>
+        <div class="eyeCatcher">
+                <h2>Denk jij ook wel eens een vvan deze dingen?</h2>
+                <div class="eyes">
+                        <img src="./images/luca.png" alt="eye1">
+                        <img src="./images/Tasnim.png" alt="eye2">
+                        <img src="./images/Sarah.png" alt="eye3">
+                </div>
+        </div>
+</div>
+<div class="content">
+        <div class="text1">
+                <h2>Dan ben jij op het juiste adres!</h2>
+                <p>Wij zijn een community van jongeren die allemaal met dit soort vragen 
+                worstelen. Binnen de Transformers Community delen we onze ervaringen, 
+                steunen elkaar én leren van mekaar.
+                <br>
+                De Transformers Community is een plek waar je écht jezelf mag zijn. Een 
+                plek waar je leert hoe je onzekerheden en tegenslagen omzet in kracht, en 
+                iets meer schijt hebt aan de mening van anderen ;-).
+                </p>
+        </div>
+        <div class="video">
+                <!-- Hier komt een youtube video -->
+        </div>
+        <div class="text2">
+                <h2>Wat kan jij bij ons doen?</h2>
+                <p>
+                -Jouw verhaal delen over alles op het gebied van persoonlijke 
+                ontwikkeling en mentale gezondheid.
+                <br>
+                -Ervaringen van anderen lezen over de uitdagingen die zij 
+                hebben overwonnen in hun leven én hun advies voor jou. 
+                <br>
+                -Wederzijds support verwachten. We helpen elkaar om te
+                groeien en leren van elkaar.
+                <br>
+                -Leren van experts die de beste tips geven op het gebied van 
+                persoonlijke ontwikkeling die je direct kan toepassen in je eigen 
+                leven. 
+                <br>
+                -Anoniem jouw vraag stellen aan één van onze coaches. Iedere
+                twee weken beantwoorden coaches Estrella, Naouel en Tim jouw 
+                anonieme vraag.  
+                </p>
+        </div>
+        <div class="text3">
+                <div class="text4">
+                        <h2>Privacy</h2>
+                        <p>Dat betekent dat al jouw informatie binnen de community blijft 
+                        en je data niet stiekem wordt doorverkocht. 
+                        <br>
+                        Note: Je kan je niet anoniem aanmelden. Wel kan je onder een andere naam 
+                        aanmelden als je liever niet je echte naam gebruikt. </p>
+                </div>
+                <div class="text5">
+                        <h2>Keep up with us!</h2>
+
+                        <p> Transformers Community is powered by Mighty Networks</p>
+                        <p>Phone numbers from outside the United States may 
+                        need to use a prefix (e.g. +44 XX XXXX XXXX). Available 
+                        on iOS and Android. This site is protected by reCAPTCHA 
+                        and the Google Privacy Policy and Terms of Service apply.</p>
+                </div>
+        </div>
+
+</div>
+<div class="footer"> 
+        <img src="./tc-b.png" alt="logo">
+        <h3>Mighty networks</h3>
+        <h3>Terms of use - Privacy Policy</h3>
 </div>
 <?php $this->stop()?>
 
